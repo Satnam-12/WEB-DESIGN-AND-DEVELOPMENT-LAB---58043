64a577f95c144e1bfd7f43aeb019bc247086e7ca
Satnam Singh Nazran <89491668+Satnam-12@users.noreply.github.com>
2022-09-30 22:33:15 +0800

    Add files via upload

---
+++ Exercise 1.php
@@ -0,0 +1,33 @@
+<html>
+<head>
+<title> Exercise 1 WDD </title>
+<head>
+<body>
+<center>
+
+<?php
+     
+	 //declare variables
+	 $name = "Satnam Singh Nazran";
+	 $college_program = "CPE";
+	 $year = "3rd Year";
+	 $section = "58043";
+	 $string = "Today, Iwill learn PHP programming";
+	 
+	 //print variables
+	 echo "$name";
+	 echo "<br>";
+	 echo "$college_program";
+	 echo "<br>";
+	 echo "$year";
+	 echo "<br>";
+	 echo "$section";
+	 echo "<br>";
+	 echo "$string";
+	 echo "<br>";
+	 
+	 ?>
+	 
+	 
+</body>
+</html>
